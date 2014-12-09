dbfbf14cd0a5d73726329571fac2be458d0597b4
Lucas <lucas@agtlucas.com>
2014-12-09 07:50:55 -0200

    Update app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/mongotest');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  var kittySchema = mongoose.Schema({
+    name: string
+  });
+
+  var Kitten = mongoose.Model('Kitten', kittySchema);
+
+  var silence = new Kitten({name: 'Silence'});
+  silence.save(function (err) {
+    if (err)
+      console.error('Meow');
+  })
+  console.log(silence.name);
+});
